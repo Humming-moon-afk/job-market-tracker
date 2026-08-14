64139d99da7fa5bc7289ae7344ecf097045069f0
Humming-Moon-afk <milian.loechner@gmail.com>
2026-08-14 20:43:57 +0200

    refactor(): implement raise_for_status in fetch_page()

--- job-market-tracker/scraper.py
+++ job-market-tracker/scraper.py
@@ -1,16 +1,17 @@
 import os
 import psycopg2
+import requests
 
 DB_CONFIG = {
     "dbname": os.getenv("DB_NAME", "fhirdb"),
-    "user": os.getenv("DB_USER", "postgre"),
-    "password": os.getenv("DB_PASSWORD", "postgre"),
+    "user": os.getenv("DB_USER", "postgres"),
+    "password": os.getenv("DB_PASSWORD", "postgres"),
     "host": os.getenv("DB_HOST", "localhost"),
     "port": os.getenv("DB_PORT", "5432")
 }
 
 
-def create_db():
+def create_table():
     with psycopg2.connect(**DB_CONFIG) as connector:
         with connector.cursor() as cursor:
             cursor.execute("""CREATE TABLE IF NOT EXISTS jobs(
@@ -21,4 +22,16 @@ def create_db():
             company VARCHAR(255)
             );
             """)
-create_db()
+
+def fetch_page(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+    try:
+        response = requests.get(url, headers=headers, timeout=5)
+        response.raise_for_status()
+        return response.text
+    except Exception as e:
+        print(f"Fehler: {e}")
+        return None
+    
